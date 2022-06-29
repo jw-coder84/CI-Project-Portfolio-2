a0ee708bafee42261d569f89656941b4b8c04c39
jw-coder84 <jmw_19@hotmail.com>
2022-06-29 14:40:48 +0000

    Added ids to form labels, added function to display question and answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,20 @@
-console.log('connected');
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    displayQuestion();
+
+
+})
+
+
+function displayQuestion() {
+    let Question1 = {'Which film won all 11 oscars that it was nominated for?': ['Titanic', 'Forrest Gump', 'The Lord of the Rings: The Return of the King', 'West Side Story (2021)']};
+
+
+    document.getElementById('question').innerText = Object.keys(Question1);
+    document.getElementById('a1').innerText = Object.values(Question1[0]);
+    document.getElementById('a2').innerText = Object.values(Question1[1]);
+    
+
+
+}
