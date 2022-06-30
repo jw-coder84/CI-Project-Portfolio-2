e2225989e96b131d4d8005a639d332f3b147bfce
jw-coder84 <jmw_19@hotmail.com>
2022-06-30 14:58:57 +0000

    Minor changes to question form html and styling, added more to display question function

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,13 +8,27 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 function displayQuestion() {
-    let Question1 = {'Which film won all 11 oscars that it was nominated for?': ['Titanic', 'Forrest Gump', 'The Lord of the Rings: The Return of the King', 'West Side Story (2021)']};
+    let qA = {question: 'Which film won all 11 oscars that it was nominated for?', answers: ['Titanic', 'Forrest Gump', 'The Lord of the Rings: The Return of the King', 'West Side Story (2021)'],
+            question2: 'Which 2 James Bond films were directed by Martin Campbell?', answers2: ['GoldenEye and Casino Royale', 'The World is Not Enough and Quantum of Solace', 'The Living Daylights and Liscence to Kill', 'Skyfall and Spectre'],
+            };
 
 
-    document.getElementById('question').innerText = Object.keys(Question1);
-    document.getElementById('a1').innerText = Object.values(Question1[0]);
-    document.getElementById('a2').innerText = Object.values(Question1[1]);
+
+    document.getElementById('question').innerText = qA.question;
+    document.getElementById('a0').innerText = qA.answers[0];
+    document.getElementById('a1').innerText = qA.answers[1];
+    document.getElementById('a2').innerText = qA.answers[2];
+    document.getElementById('a3').innerText = qA.answers[3];
     
+    document.getElementsByClassName('btn-nextq').addEventListener('click', function() {
+        document.getElementById('question').innerText = qA.question2;
+        document.getElementById('a0').innerText = qA.answers2[0];
+        document.getElementById('a1').innerText = qA.answers2[1];
+        document.getElementById('a2').innerText = qA.answers2[2];
+        document.getElementById('a3').innerText = qA.answers2[3];
+
+    })
+
 
 
 }
