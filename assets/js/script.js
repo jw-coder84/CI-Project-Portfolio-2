c1d6c0c03ced6a160e31d1bf30dd57f8b1db7258
jw-coder84 <jmw_19@hotmail.com>
2022-06-30 17:16:25 +0000

    Changed data structure for questions and answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,28 +7,29 @@ document.addEventListener("DOMContentLoaded", function () {
 })
 
 
+
+let qA = [{
+    question: 'Which film won all 11 oscars that it was nominated for?', 
+    answerOne: 'Titanic', 
+    answerTwo: 'Forrest Gump', 
+    rightAnswer: 'The Lord of the Rings: The Return of the King', 
+    answerThree: 'West Side Story (2021)'},
+{
+    question: 'Which 2 James Bond films were directed by Martin Campbell?', 
+    rightAnswer: 'GoldenEye and Casino Royale', 
+    answerOne: 'The World is Not Enough and Quantum of Solace', 
+    answerTwo: 'The Living Daylights and Liscence to Kill', 
+    answerThree: 'Skyfall and Spectre',
+}];
+
+
 function displayQuestion() {
-    let qA = {question: 'Which film won all 11 oscars that it was nominated for?', answers: ['Titanic', 'Forrest Gump', 'The Lord of the Rings: The Return of the King', 'West Side Story (2021)'],
-            question2: 'Which 2 James Bond films were directed by Martin Campbell?', answers2: ['GoldenEye and Casino Royale', 'The World is Not Enough and Quantum of Solace', 'The Living Daylights and Liscence to Kill', 'Skyfall and Spectre'],
-            };
 
-
-
-    document.getElementById('question').innerText = qA.question;
-    document.getElementById('a0').innerText = qA.answers[0];
-    document.getElementById('a1').innerText = qA.answers[1];
-    document.getElementById('a2').innerText = qA.answers[2];
-    document.getElementById('a3').innerText = qA.answers[3];
-    
-    document.getElementsByClassName('btn-nextq').addEventListener('click', function() {
-        document.getElementById('question').innerText = qA.question2;
-        document.getElementById('a0').innerText = qA.answers2[0];
-        document.getElementById('a1').innerText = qA.answers2[1];
-        document.getElementById('a2').innerText = qA.answers2[2];
-        document.getElementById('a3').innerText = qA.answers2[3];
-
-    })
-
+    document.getElementById('question').innerText = qA[0].question;
+    document.getElementById('a0').innerText = qA[0].answerOne;
+    document.getElementById('a1').innerText = qA[0].answerTwo;
+    document.getElementById('a2').innerText = qA[0].rightAnswer;
+    document.getElementById('a3').innerText = qA[0].answerThree;
 
 
 }
