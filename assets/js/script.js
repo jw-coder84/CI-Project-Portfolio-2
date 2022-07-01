13c2025c84f9760c07b87391f44fce555d60a988
jw-coder84 <jmw_19@hotmail.com>
2022-07-01 13:23:17 +0000

    Completed question and answers object

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,34 +9,114 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 let qA = [{
-    question: 'Which film won all 11 oscars that it was nominated for?', 
-    answerOne: 'Titanic', 
-    answerTwo: 'Forrest Gump', 
-    rightAnswer: 'The Lord of the Rings: The Return of the King', 
-    answerThree: 'West Side Story (2021)'},
+    question: 'Which film won all eleven oscars that it was nominated for?', 
+    answers: { 
+        a: 'Titanic', 
+        b: 'Forrest Gump', 
+        c: 'The Lord of the Rings: The Return of the King', 
+        d: 'West Side Story (2021)'
+    },
+    correctAnswer: 'c'
+},
 {
     question: 'Which 2 James Bond films were directed by Martin Campbell?', 
-    rightAnswer: 'GoldenEye and Casino Royale', 
-    answerOne: 'The World is Not Enough and Quantum of Solace', 
-    answerTwo: 'The Living Daylights and Liscence to Kill', 
-    answerThree: 'Skyfall and Spectre',
+    answers: { 
+        a: 'GoldenEye and Casino Royale', 
+        b: 'The World is Not Enough and Quantum of Solace', 
+        c: 'The Living Daylights and Liscence to Kill', 
+        d: 'Skyfall and Spectre'
+    },
+    correctAnswer: 'a'
 },
 {
     question: 'Which film did Dan Aykroyd star in and NOT co-write?',
-    answerOne: 'The Blues Brothers',
-    rightAnswer: 'Evolution',
-    answerTwo: 'Ghostbusters',
-    answerThree: 'Ghostbusters 2'
+    answers: {
+        a: 'The Blues Brothers',
+        b: 'Evolution',
+        c: 'Ghostbusters',
+        d: 'Ghostbusters 2'
+    },
+    correctAnswer: 'b'
+},
+{
+    question: 'In The Terminator, who was originally cast to play the title character?',
+    answers: {
+        a: 'Lance Henriksen',
+        b: 'Michael Biehn',
+        c: 'Brian Thompson',
+        d: 'Paul Winfield'
+    },
+    correctAnswer: 'a'
+},
+{
+    question: 'In Back to the Future Part 2, which date in the past do Marty and doc Brown travel to?',
+    answers: {
+        a: 'September 2nd 1885',
+        b: 'January 1st 1885',
+        c: 'October 21st 1955',
+        d: 'November 12th 1955'
+    },
+    correctAnswer: 'd'
+},
+{
+    question: 'Which Mission Impossible film did J.J Abrams co-write and direct',
+    answers: {
+        a: 'I',
+        b: 'II',
+        c: 'III',
+        d: 'IV'
+    },
+    correctAnswer: 'c'
+},
+{
+    question: 'What was the release year of the first Jurassic Park film',
+    answers: {
+        a: '1991',
+        b: '1992',
+        c: '1993',
+        d: '1994'
+    },
+    correctAnswer: 'c'
+},
+{
+    question: 'Who played the title character in Tim Burton\'s Beetlejuice?',
+    answers: {
+        a: 'Johnny Depp',
+        b: 'Christopher Walken',
+        c: 'Danny DeVito',
+        d: 'Michael Keaton'
+    },
+    correctAnswer: 'd'
+},
+{
+    question: 'In Christopher Nolan\'s Batman Trilogy, which US city was used to represent Gotham?',
+    answers: {
+        a: 'Chicago',
+        b: 'New York',
+        c: 'Los Angeles',
+        d: 'Detroit'
+    },
+    correctAnswer: 'a'
+},
+{
+    question: 'How many Die Hard films were directed by John McTiernan?',
+    answers: {
+        a: '1',
+        b: '2',
+        c: '3',
+        d: '4'
+    },
+    correctAnswer: 'b'
 }];
 
 
 function displayQuestion() {
 
     document.getElementById('question').innerText = qA[0].question;
-    document.getElementById('a0').innerText = qA[0].answerOne;
-    document.getElementById('a1').innerText = qA[0].answerTwo;
-    document.getElementById('a2').innerText = qA[0].rightAnswer;
-    document.getElementById('a3').innerText = qA[0].answerThree;
+    document.getElementById('a0').innerText = qA[0].answers.a;
+    document.getElementById('a1').innerText = qA[0].answers.b;
+    document.getElementById('a2').innerText = qA[0].answers.c;
+    document.getElementById('a3').innerText = qA[0].answers.d;
 
 
 }
@@ -44,9 +124,9 @@ function displayQuestion() {
 document.getElementsByClassName('btn-nextq').addEventListener('click', function() {
     for (let i = 0; i <= qA.length; i ++) {
         document.getElementById('question').innerText = qA[i].question;
-        document.getElementById('a0').innerText = qA[i].answerOne;
-        document.getElementById('a1').innerText = qA[i].answerTwo;
-        document.getElementById('a2').innerText = qA[i].rightAnswer;
-        document.getElementById('a3').innerText = qA[i].answerThree;
+        document.getElementById('a0').innerText = qA[i].answers.a;
+        document.getElementById('a1').innerText = qA[i].answers.b;
+        document.getElementById('a2').innerText = qA[i].answers.c;
+        document.getElementById('a3').innerText = qA[i].answers.d;
     }
 })
